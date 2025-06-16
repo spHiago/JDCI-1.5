ec3fbcca6d8b9e91fe66f043cc16797a95b5f071
Hiago Franco <hiago.franco@students.assembly.pt>
2025-06-16 19:50:42 +0100

    ex01_Finished

---
+++ ex01_LowtoLargest/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 1. The application receives five numbers and returns them ordered from lowest to largest.
+The numbers before being displayed must be stored in an array;*/
+
+Console.WriteLine("Digite 5 numeros e eu lhe devolverei em um array em ordem crescente: (obs, aperte (Enter) apos cada numero digitado)");
+
+int[] array = new int[5];
+int i = 0;
+int j = 0;
+
+while (i < 5)
+{
+    array[i] = int.Parse(Console.ReadLine());
+    i++;
+}
+
+Array.Sort(array);
+
+Console.WriteLine("Otimo, aqui esta o seu array em ordem crescente:");
+while (j < 5)
+{
+    Console.WriteLine(array[j]);
+    j++;
+}
